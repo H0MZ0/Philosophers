789ce38d91f8f3a549b29351aee01fcc233469a9
H0MZ0 <hamzared2500@gmail.com>
2025-06-20 12:20:41 +0100

    test

--- philo/main.c
+++ philo/main.c
@@ -6,7 +6,7 @@
 /*   By: hakader <hakader@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/17 12:22:49 by hakader           #+#    #+#             */
-/*   Updated: 2025/06/20 11:51:09 by hakader          ###   ########.fr       */
+/*   Updated: 2025/06/20 12:19:58 by hakader          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,29 @@ void take_forks(t_philo *philo)
 	print_action(philo, "has taken a fork");
 }
 
-void	init_data(t_data *data)
+void put_down_forks(t_philo *philo)
+{
+	pthread_mutex_unlock(&philo->data->forks[philo->left_fork]);
+	pthread_mutex_unlock(&philo->data->forks[philo->right_fork]);
+}
+
+void print_action(t_philo *philo, char *msg)
+{
+	pthread_mutex_lock(&philo->data->print_mutex);
+	if (!philo->data->stop)
+		printf("%d %d %s\n", current_time() - philo->data->start_time, philo->id, msg);
+	pthread_mutex_unlock(&philo->data->print_mutex);
+}
+
+int current_time(void)
+{
+	struct timeval	tv;
+	gettimeofday(&tv, NULL);
+	return ((tv.tv_sec * 1000LL) + (tv.tv_usec / 1000));
+}
+
+
+void init_data(t_data *data)
 {
 	int	i;
 	int	n;
@@ -44,6 +66,7 @@ void	init_data(t_data *data)
 		if (pthread_mutex_init(&data->forks[i], NULL) == -1)
 			put_error("pthread_mutex_init failed");
 	pthread_mutex_init(&data->print_mutex, NULL);
+	pthread_mutex_init(&data->stop_mutex, NULL);
 	i = -1;
 	while (++i < n)
 	{
@@ -53,13 +76,12 @@ void	init_data(t_data *data)
 		data->philos[i].meals_eaten = 0;
 		data->philos[i].last_meal_time = 0;
 		data->philos[i].data = data;
+		pthread_mutex_init(&data->philos[i].meal_mutex, NULL);
 	}
 	data->stop = 0;
-	if (pthread_mutex_init(&data->stop_mutex, NULL) != 0)
-		put_error("pthread_mutex_init failed (stop_mutex)");
 }
 
-void	init_args(int ac, char **av, t_args *args)
+void init_args(int ac, char **av, t_args *args)
 {
 	if (ac < 5 || ac > 6)
 		put_error("Invalid number of arguments");
@@ -74,123 +96,154 @@ void	init_args(int ac, char **av, t_args *args)
 	if (ac == 6)
 	{
 		args->num_meals = ft_atoi(av[5]);
-		if (args->num_meals < 0)
+		if (args->num_meals <= 0)
 			put_error("Invalid number of meals");
 	}
 }
 
-long long	current_time(void)
-{
-	struct timeval	tv;
-
-	gettimeofday(&tv, NULL);
-	return ((tv.tv_sec * 1000LL) + (tv.tv_usec / 1000));
-}
-
-void	print_action(t_philo *philo, char *msg)
-{
-	pthread_mutex_lock(&philo->data->print_mutex);
-	printf("%lld %d %s\n", current_time() - philo->data->start_time, philo->id, msg);
-	pthread_mutex_unlock(&philo->data->print_mutex);
-}
-
-void put_down_forks(t_philo *philo)
-{
-	pthread_mutex_unlock(&philo->data->forks[philo->left_fork]);
-	pthread_mutex_unlock(&philo->data->forks[philo->right_fork]);
-}
-
 void *philo_routine(void *arg)
 {
-    t_philo *philo = (t_philo *)arg;
-    if (philo->data->args.num_philos == 1)
-    {
-        pthread_mutex_lock(&philo->data->forks[philo->left_fork]);
-        print_action(philo, "has taken a fork");
-        sleep_ms(philo->data->args.time_to_die);
-        pthread_mutex_unlock(&philo->data->forks[philo->left_fork]);
-        return NULL;
-    }
-    while (!philo->data->stop)
-    {
-        // If we have a meal limit and this philosopher has finished, exit
-        if (philo->data->args.num_meals > 0 &&
-            philo->meals_eaten >= philo->data->args.num_meals)
-            break;
+	t_philo *philo = (t_philo *)arg;
 
-        print_action(philo, "is thinking");
-        take_forks(philo);
-        print_action(philo, "is eating");
-        philo->last_meal_time = current_time();
-        philo->meals_eaten++;
-        sleep_ms(philo->data->args.time_to_eat);
-        put_down_forks(philo);
-        print_action(philo, "is sleeping");
-        sleep_ms(philo->data->args.time_to_sleep);
-    }
-    return NULL;
+	if (philo->data->args.num_philos == 1)
+	{
+		pthread_mutex_lock(&philo->data->forks[philo->left_fork]);
+		print_action(philo, "has taken a fork");
+		sleep_ms(philo->data->args.time_to_die);
+		print_action(philo, "died");
+		pthread_mutex_unlock(&philo->data->forks[philo->left_fork]);
+		return NULL;
+	}
+	while (1)
+	{
+		pthread_mutex_lock(&philo->data->stop_mutex);
+		int stop = philo->data->stop;
+		pthread_mutex_unlock(&philo->data->stop_mutex);
+		if (stop)
+			break;
+
+		pthread_mutex_lock(&philo->meal_mutex);
+		if (philo->data->args.num_meals > 0 &&
+			philo->meals_eaten >= philo->data->args.num_meals)
+		{
+			pthread_mutex_unlock(&philo->meal_mutex);
+			break;
+		}
+		pthread_mutex_unlock(&philo->meal_mutex);
+
+		print_action(philo, "is thinking");
+		take_forks(philo);
+		print_action(philo, "is eating");
+
+		pthread_mutex_lock(&philo->meal_mutex);
+		philo->last_meal_time = current_time();
+		philo->meals_eaten++;
+		pthread_mutex_unlock(&philo->meal_mutex);
+
+		sleep_ms(philo->data->args.time_to_eat);
+		put_down_forks(philo);
+		print_action(philo, "is sleeping");
+		sleep_ms(philo->data->args.time_to_sleep);
+	}
+	return NULL;
 }
 
 void *monitor_routine(void *arg)
 {
 	t_data *data = (t_data *)arg;
-	int i, done;
-	i = 0;
-	while (!data->stop)
+	int	i;
+	int	done;
+
+	while (1)
 	{
+		usleep(1000);
+		done = 1;
 		i = 0;
 		while (i < data->args.num_philos)
 		{
-			if (current_time() - data->philos[i].last_meal_time > data->args.time_to_die)
+			pthread_mutex_lock(&data->philos[i].meal_mutex);
+			long long time_since_meal = current_time() - data->philos[i].last_meal_time;
+			int meals = data->philos[i].meals_eaten;
+			pthread_mutex_unlock(&data->philos[i].meal_mutex);
+
+			pthread_mutex_lock(&data->stop_mutex);
+			int stop = data->stop;
+			pthread_mutex_unlock(&data->stop_mutex);
+			if (stop)
+				return NULL;
+
+			if (time_since_meal > data->args.time_to_die)
 			{
-				if (data->args.num_philos != 1)
+				pthread_mutex_lock(&data->stop_mutex);
+				if (!data->stop)
+				{
+					data->stop = 1;
 					print_action(&data->philos[i], "died");
-				data->stop = 1;
-				pthread_detach(data->philos[0].thread_id);
-				break;
+				}
+				pthread_mutex_unlock(&data->stop_mutex);
+				return NULL;
 			}
+			if (data->args.num_meals > 0 && meals < data->args.num_meals)
+				done = 0;
 			i++;
 		}
-		if (data->args.num_meals > 0)
+		if (data->args.num_meals > 0 && done)
 		{
-			done = 1;
-			i = 0;
-			while (i++ < data->args.num_philos)
-				if (data->philos[i].meals_eaten < data->args.num_meals)
-					done = 0;
-			if (done)
-				data->stop = 1;
+			pthread_mutex_lock(&data->stop_mutex);
+			data->stop = 1;
+			pthread_mutex_unlock(&data->stop_mutex);
+			return NULL;
 		}
-		usleep(1000);
 	}
 	return NULL;
 }
 
+void cleanup(t_data *data)
+{
+	int i;
+	for (i = 0; i < data->args.num_philos; i++)
+	{
+		pthread_mutex_destroy(&data->forks[i]);
+		pthread_mutex_destroy(&data->philos[i].meal_mutex);
+	}
+	pthread_mutex_destroy(&data->print_mutex);
+	pthread_mutex_destroy(&data->stop_mutex);
+	free(data->philos);
+	free(data->forks);
+}
+
 int main(int ac, char **av)
 {
-    int         i;
-    t_data      data;
-    pthread_t   monitor_thread;
+	int			i;
+	t_data		data;
+	pthread_t	monitor_thread;
 
-    init_args(ac, av, &data.args);
-    init_data(&data);
+	init_args(ac, av, &data.args);
+	init_data(&data);
 
-    data.start_time = current_time();
-    for (i = 0; i < data.args.num_philos; i++)
-        data.philos[i].last_meal_time = data.start_time;
+	data.start_time = current_time();
+	for (i = 0; i < data.args.num_philos; i++)
+	{
+		pthread_mutex_lock(&data.philos[i].meal_mutex);
+		data.philos[i].last_meal_time = data.start_time;
+		pthread_mutex_unlock(&data.philos[i].meal_mutex);
+	}
 
-    i = 0;
-    while (i < data.args.num_philos)
-    {
-        if (pthread_create(&data.philos[i].thread_id, NULL, philo_routine, &data.philos[i]) != 0)
-            put_error("Failed to create thread");
-        i++;
-    }
-    if (pthread_create(&monitor_thread, NULL, monitor_routine, &data) != 0)
-        put_error("Failed to create monitor thread");
-    i = 0;
-    while (i < data.args.num_philos)
-        pthread_join(data.philos[i++].thread_id, NULL);
-    pthread_join(monitor_thread, NULL);
-    return (0);
+	i = 0;
+	while (i < data.args.num_philos)
+	{
+		if (pthread_create(&data.philos[i].thread_id, NULL, philo_routine, &data.philos[i]) != 0)
+			put_error("Failed to create thread");
+		i++;
+	}
+	if (pthread_create(&monitor_thread, NULL, monitor_routine, &data) != 0)
+		put_error("Failed to create monitor thread");
+
+	i = 0;
+	while (i < data.args.num_philos)
+		pthread_join(data.philos[i++].thread_id, NULL);
+	pthread_join(monitor_thread, NULL);
+
+	cleanup(&data);
+	return (0);
 }
--- philo/philo.h
+++ philo/philo.h
@@ -5,40 +5,40 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: hakader <hakader@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/06/17 12:23:59 by hakader           #+#    #+#             */
-/*   Updated: 2025/06/19 19:01:19 by hakader          ###   ########.fr       */
+/*   Created: 2025/06/20 11:54:57 by hakader           #+#    #+#             */
+/*   Updated: 2025/06/20 12:18:15 by hakader          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
 # define PHILO_H
 
-# include <stdio.h>
-# include <unistd.h>
-# include <stdlib.h>
-# include <string.h>
 # include <pthread.h>
+# include <stdio.h>
+# include <stdlib.h>
 # include <sys/time.h>
+# include <unistd.h>
 
 typedef struct s_args
 {
-	int		num_philos;
-	int		time_to_die;
-	int		time_to_eat;
-	int		time_to_sleep;
-	int		num_meals;
+	int	num_philos;
+	int	time_to_die;
+	int	time_to_eat;
+	int	time_to_sleep;
+	int	num_meals;
 }	t_args;
 
 typedef struct s_data	t_data;
 
 typedef struct s_philo
 {
-	pthread_t		thread_id;
 	int				id;
+	int				meals_eaten;
 	int				left_fork;
 	int				right_fork;
-	int				meals_eaten;
-	long			last_meal_time;
+	int				last_meal_time;
+	pthread_mutex_t	meal_mutex;
+	pthread_t		thread_id;
 	t_data			*data;
 }	t_philo;
 
@@ -48,16 +48,23 @@ typedef struct s_data
 	t_philo			*philos;
 	pthread_mutex_t	*forks;
 	pthread_mutex_t	print_mutex;
-	long long		start_time;
+	pthread_mutex_t	stop_mutex;
 	int				stop;
-	pthread_mutex_t stop_mutex;
+	int				start_time;
 }	t_data;
 
-long long	current_time(void);
-void	print_action(t_philo *philo, char *msg);
+// Utility functions
 int		ft_atoi(const char *str);
-void	put_error(char *message);
-void	init_data(t_data *data);
-void	init_args(int ac, char **av, t_args *args);
+void	put_error(char *msg);
+int		current_time(void);
+void	print_action(t_philo *philo, char *msg);
+void *philo_routine(void *arg);
+void *monitor_routine(void *arg);
+void init_args(int ac, char **av, t_args *args);
+void init_data(t_data *data);
+// void cleanup(t_data *data);
+void put_down_forks(t_philo *philo);
+void sleep_ms(int ms);
+void take_forks(t_philo *philo);
 
 #endif
